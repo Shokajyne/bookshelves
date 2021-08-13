d0e3a818fcd66edc8f7cd95f491a8a5c5a3856a5
Chris RICHARD <chris.richard3@gmail.com>
2021-08-13 17:36:33 +0200

    book forms

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/models/Book.model';
+import { BooksService } from 'src/app/services/books.service';
 
 @Component({
   selector: 'app-book-form',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFormComponent implements OnInit {
 
-  constructor() { }
+  bookForm: FormGroup = this.formBuilder.group({
+    title: ['', Validators.required],
+    author: ['', Validators.required]
+  });;
+
+  constructor(private formBuilder: FormBuilder,
+              private bookService: BooksService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onSaveBook() {
+    const formValue = this.bookForm.value;
+    const newBook = new Book(
+      formValue['title'], 
+      formValue['author']
+    );
+
+    this.bookService.createNewBook(newBook);
+
+    this.router.navigate(['/books']);
+  }
 }
